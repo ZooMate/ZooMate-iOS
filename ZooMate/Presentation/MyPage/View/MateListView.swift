84e19ee19a828d513e46820927f5c313278ccc13
Qnokka <106862472+Qnokka@users.noreply.github.com>
2025-05-26 17:24:58 +0900

    Feat: [MyPage/Mate] 관심 반려동물 목록 UI 레이아웃 구현

---
+++ ZooMate/Presentation/MyPage/View/MateListView.swift
@@ -0,0 +1,18 @@
+//
+//  MateListView.swift
+//  ZooMate
+//
+//  Created by 최준영 on 5/26/25.
+//
+
+import SwiftUI
+
+struct MateListView: View {
+    var body: some View {
+        MateView(data: DummyData1())
+    }
+}
+
+#Preview {
+    MateListView()
+}
---
+++ ZooMate/Presentation/MyPage/View/MateView.swift
@@ -0,0 +1,38 @@
+//
+//  Mate.swift
+//  ZooMate
+//
+//  Created by 최준영 on 5/26/25.
+//
+
+import SwiftUI
+
+struct MateView: View {
+    
+    @StateObject var data = DummyData1()
+    
+    let columns = [
+        GridItem(.flexible(), spacing: 16),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 16) {
+                    ForEach(matedPets) { pet in
+                        PetCardCell(pet: pet)
+                    }
+                }
+                .padding(16)
+                .navigationTitle("메이트")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+        }
+        
+    }
+    
+    private var matedPets: [Pet] {
+        data.dummyPets
+    }
+}
