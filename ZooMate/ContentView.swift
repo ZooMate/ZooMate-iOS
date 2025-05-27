7781ce21300de0eb83c9ffe5f8b22342a490da5b
ksiomng <nadana0929@gmail.com>
2025-05-27 12:28:49 +0900

    Fix: 전역으로 적용해야하는 백버튼은 앱의 최상단으로 이동

--- ZooMate/ContentView.swift
+++ ZooMate/ContentView.swift
@@ -24,6 +24,31 @@ struct ContentView: View {
         .onAppear {
             UITabBar.appearance().unselectedItemTintColor = .subText
         }
+        .onAppear {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithTransparentBackground()
+            
+            let backItemAppearance = UIBarButtonItemAppearance()
+            backItemAppearance.normal.titleTextAttributes = [
+                .foregroundColor : UIColor.clear
+            ]
+            appearance.backButtonAppearance = backItemAppearance
+            
+            let image = UIImage(systemName: "chevron.backward")?
+                .withTintColor(
+                    .mainText,
+                    renderingMode: .alwaysOriginal
+                )
+                .withAlignmentRectInsets(
+                    UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 0)
+                )
+            appearance.setBackIndicatorImage(image, transitionMaskImage: image)
+
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+            UINavigationBar.appearance().compactAppearance = appearance
+            UINavigationBar.appearance().compactScrollEdgeAppearance = appearance
+        }
     }
 }
 
