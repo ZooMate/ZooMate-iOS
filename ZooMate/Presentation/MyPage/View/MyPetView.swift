7c5ea65282dcb29525c3415f8e17dd00f25ac5b3
Qnokka <106862472+Qnokka@users.noreply.github.com>
2025-05-28 02:29:24 +0900

    Fix: MyPetList 뷰 중첩 수정

--- ZooMate/Presentation/MyPage/View/MyPetView.swift
+++ ZooMate/Presentation/MyPage/View/MyPetView.swift
@@ -17,26 +17,25 @@ struct MyPetView: View {
     ]
     // MARK: 등록한 반려동물의 상세 프로필 entry에 따른 버튼 출력 여부
     var body: some View {
-        NavigationStack {
-            ScrollView {
-                LazyVGrid(columns: columns, spacing: 16) {
-                    ForEach(pets, id: \.petId) { pet in
-                        NavigationLink (destination: MyPetDetailView(pet: pet)
-                            .onAppear {
-                                isOnDetail = true
-                            }
-                            .onDisappear {
-                                isOnDetail = false
-                            }){
-                                PetCardCell(pet: pet)
-                            }
-                    }
+        //NavigationStack {
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 16) {
+                ForEach(pets, id: \.petId) { pet in
+                    NavigationLink (destination: MyPetDetailView(pet: pet)
+                        .onAppear {
+                            isOnDetail = true
+                        }
+                        .onDisappear {
+                            isOnDetail = false
+                        }){
+                            PetCardCell(pet: pet)
+                        }
                 }
-                .padding(16)
-                .navigationTitle("내 반려동물")
-                .navigationBarTitleDisplayMode(.inline)
             }
+            .padding(16)
+            .navigationTitle("내 반려동물")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        
+        //}
     }
 }
