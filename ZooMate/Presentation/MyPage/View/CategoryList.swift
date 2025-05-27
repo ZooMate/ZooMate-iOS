577fc7e290c913770a884b049540960d4885da80
Qnokka <106862472+Qnokka@users.noreply.github.com>
2025-05-28 02:34:22 +0900

    Feat: enum 바인딩 버튼 목록으로 수정

--- ZooMate/Presentation/MyPage/View/CategoryList.swift
+++ ZooMate/Presentation/MyPage/View/CategoryList.swift
@@ -10,49 +10,42 @@ import SwiftUI
 struct CategoryList: View {
     
     @Binding var showCategory: Bool
-    // MARK: 반려등록 뷰 - 카테고리 목록
+    
+    var onSelect: (Category) -> Void
+
     var body: some View {
-        
-        ZStack {
-            
-            Color.black.opacity(0.4)
-                .ignoresSafeArea()
-                .onTapGesture {
-                    withAnimation {
-                        showCategory = false
+            ZStack {
+                Color.black.opacity(0.4)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        withAnimation {
+                            showCategory = false
+                        }
+                    }
+
+                VStack(spacing: 15) {
+                    ForEach([Category.bird, .reptile, .cat, .dog], id: \.self) { category in
+                        Button {
+                            withAnimation {
+                                showCategory = false
+                            }
+                            onSelect(category)
+                        } label: {
+                            Text(category.rawValue)
+                                .font(.notoSansMedium(size: 18))
+                                .frame(maxWidth: .infinity)
+                        }
                     }
                 }
-            
-            VStack(spacing: 15) {
-                Button {
-                } label: {
-                    Text("조류")
-                }
-                Button {
-                    
-                } label: {
-                    Text("파충류")
-                }
-                Button {
-                    
-                } label: {
-                    Text("고양이")
-                }
-                Button {
-                    
-                } label: {
-                    Text("강아지")
-                }
+                .padding(15)
+                .padding(.horizontal, 60)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+                .foregroundStyle(.mainText)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+                .padding(35)
+                .padding(.bottom, 30)
+                .padding(.horizontal, 30)
             }
-            .padding(15)
-            .padding(.horizontal, 60)
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 15))
-            .foregroundStyle(.mainText)
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
-            .padding(35)
-            .padding(.bottom, 30)
-            .padding(.horizontal, 30)
         }
     }
-}
