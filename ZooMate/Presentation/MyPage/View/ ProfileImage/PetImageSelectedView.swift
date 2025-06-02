7c2980f45431bfb106cd103541229e68f4dd2156
ksiomng <nadana0929@gmail.com>
2025-06-02 10:52:38 +0900

    Design: 사진 테두리가 살짝 잘려서 수정

--- ZooMate/Presentation/MyPage/View/ ProfileImage/PetImageSelectedView.swift
+++ ZooMate/Presentation/MyPage/View/ ProfileImage/PetImageSelectedView.swift
@@ -12,7 +12,7 @@ struct PetImageSelectedView: View {
     @Binding var selectedImages: [UIImage]
     @State private var isPickerPresented = false
 
-    private let imageSize: CGFloat = 100
+    private let imageSize: CGFloat = 120
     private let columns = Array(repeating: GridItem(.flexible(), spacing: 10), count: 3)
 
     var body: some View {
@@ -27,9 +27,10 @@ struct PetImageSelectedView: View {
                                 .fill(Color.white)
                                 .frame(width: imageSize, height: imageSize)
                                 .overlay(
-                                    RoundedRectangle(cornerRadius: 20)
+                                    RoundedRectangle(cornerRadius: 22)
                                         .stroke(.category, lineWidth: 2)
                                 )
+                                .padding(2)
                             Image(systemName: "camera")
                                 .font(.system(size: 30))
                                 .foregroundColor(.sandBeige)
@@ -43,9 +44,10 @@ struct PetImageSelectedView: View {
                             .frame(width: imageSize, height: imageSize)
                             .clipShape(RoundedRectangle(cornerRadius: 20))
                             .overlay(
-                                RoundedRectangle(cornerRadius: 20)
+                                RoundedRectangle(cornerRadius: 22)
                                     .stroke(.category, lineWidth: 2)
                             )
+                            .padding(2)
                             .clipped()
                     }
                 }
